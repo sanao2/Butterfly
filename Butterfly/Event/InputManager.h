9f2bf4c37e79d239477408b45336794d8bf82b8b
김도현 <hot99009@gmail.com>
2025-04-22 21:34:32 +0900

    Refactor: 템플릿 기반 InputManager 구조 도입 및 키보드 입력 컴포넌트 분리
    - InputManager를 템플릿 클래스 형태로 일반화
    - KeyboardDevice 특수화를 통해 키보드 입력 처리 기능 분리
    - 입력 관련 상수(KEY_PRESSED_FLAG, MAX_KEY_COUNT)를 InputConstants로 분리
    - 키보드 입력 구현 파일을 KeyboardInputManager.{h/cpp}로 분할
    - 오타 수정 및 네임스페이스 구조 정리
    
    향후 MouseDevice, GamepadDevice 등 다양한 입력 디바이스 확장 예정

--- Butterfly/Event/InputManager.h
+++ Butterfly/Event/InputManager.h
@@ -1,61 +1,14 @@
 #pragma once
 #include <Windows.h>
 #include <array>
+#include "InputConstants.h"
 
 namespace Input
 {
-    constexpr int MAX_KEY_COUNT = 256;
-    constexpr SHORT KEY_PRESSED_FLAG = 0x8000;
-
     template<typename DeviceType>
     class InputManager;
 
-    sturct KeyboardDevice {};
+    struct KeyboardDevice;
 
-    template<>
-    class InputManager<KeyboardDevice>
-    {
-      public:
-        static InputManager& GetInstance()
-        {
-          static InputManager instance;
-          return instance;
-        }
-
-        void Update()
-        {
-          std::memcpy(prevState.data(), currState.data(), sizeof(SHORT) * MAX_KEY_COUNT);
-          for(int i =0; i < MAX_KEY_COUNT; ++i)
-          {
-            currState[i] = GetAsyncKeyState(i);
-          }
-        }
-
-        bool IsKeyDown(int vKey) const
-        {
-          return (currState[vKey] & KEY_PRESSED_FLAG) != 0;
-        }
-
-        bool IsKeyPressed(int vKey) const
-        {
-          return (!(prevState[vKey] & KEY_PRESSED_FLAG) && (currState[vKey] & KEY_PRESSED_FLAG));
-        }
-
-        bool IsKeyReleased(int vKey) const
-        {
-          return ((prevState[vKey] & KEY_PRESSED_FLAG) && !(currState[vKey] & KEY_PRESSED_FLAG));
-        }
-
-        private:
-          InputManager() = default;
-          ~InputManager() = default;
-
-          InputManager(const InputManager&) = delete;
-          InputManager& oprator=(const InputManager&) = delete;
-
-          std::array<SHORT, MAX_KEY_COUNT> prevState;
-          std::array<SHORT, MAX_KEY_COUNT> currState;
-    }
-
-    using Keybard = InputManager<KeyboardDevice>;
+    using Keyboard = InputManager<KeyboardDevice>;
 }
