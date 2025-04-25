d8750afb067bd76919811c60ca0dbb3c889db2a0
sanao2 <SanaoAiza1102@gmail.com>
2025-04-25 12:33:55 +0900

    Delete Butterfly directory

--- Butterfly/Winmain/Event/InputConstants.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-namespace Input
-{
-    constexpr int MAX_KEY_COUNT = 300;          // Key Count Check
-    constexpr SHORT KEY_PRESSED_FLAG = 0x8000;  // Key Down Check 
-}
--- Butterfly/Winmain/Winmain.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <stdio.h>
-#include "MoveManagers.h"
-#include "Event/KeyboardInputManager.h"
-using namespace Input;
-
-MoveManager* Move; 
