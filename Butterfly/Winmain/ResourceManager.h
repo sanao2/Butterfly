f08fb5b99bffeecf5227ccaec8141061ba5a9a6d
Sanao2 <terry4690232@naver.com>
2025-04-28 14:38:24 +0900

    # ResourceManager.cpp / h 생성 # 클래스 생성자/ 소멸자 / 멤버 변수 생성

--- Butterfly/Winmain/ResourceManager.h
+++ Butterfly/Winmain/ResourceManager.h
@@ -5,5 +5,15 @@ using namespace std;
 
 class ResourceManager
 {
+private : 
+	HWND	hWnd; 
+	HDC		frontDC;	// FrontBuffer
+	HDC		backDC;		// BackBuffer
+	HBITMAP backBitmap; // BackBuffer Linked Bitmap
+
+public : 
+	ResourceManager(); 
+	~ResourceManager(); 
+
 };
 
