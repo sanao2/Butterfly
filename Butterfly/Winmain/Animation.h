7c881ef6890fba72b7156248d2805904d6aa1591
Sanao2 <terry4690232@naver.com>
2025-04-29 10:26:50 +0900

    # 필요한 헤더 파일 Include 추가

--- Butterfly/Winmain/Animation.h
+++ Butterfly/Winmain/Animation.h
@@ -1,4 +1,12 @@
 #pragma once
+#include <windows.h>
+#include <vector> 
+#include <iostream>
+#include <string>
+using namespace std; 
+using std::vector;
+using std::string; 
+using std::wstring; 
 
 class Animation
 {
