79e3e535781e86cb9eeead6effde27cfe171b250
Sanao2 <https://github.com/sanao2>
2025-04-29 20:56:04 +0900

    # Render.cpp / .h 파일의 RenderScene에 파라메터 HWND 삭제

--- Butterfly/Winmain/Render.cpp
+++ Butterfly/Winmain/Render.cpp
@@ -18,7 +18,7 @@ void Render::Update()
     swap->SwapBuffers();    // 백버퍼 -> 프론트버퍼 복사
     anim->Update(); // Animation Update 
 }
-void Render::RenderScene(HWND hWnd, HINSTANCE hInst)
+void Render::RenderScene(HINSTANCE hInst)
 {
     HDC memDC = swap->GetMemDC(); 
     HBITMAP tempBitmap = CreateCompatibleBitmap(GetDC(hWnd), clientSize.x, clientSize.y);
@@ -28,7 +28,7 @@ void Render::RenderScene(HWND hWnd, HINSTANCE hInst)
    PatBlt(memDC, 0, 0, clientSize.x, clientSize.y, WHITENESS);
    /*  Rectangle(memDC, 50, 50, 200, 200);*/
   // RcManager->Render(memDC, 0, 0, clientSize.x / 2, clientSize.y / 2); 
-   anim->Render( );
+   anim->Render(memDC);
 
     // 스왑 메모리 DC에 복사 (swap 내부 메모리 DC를 가져오는 메소드 필요)
    // BitBlt(GetDC(hWnd), 0, 0, clientSize.x, clientSize.y, memDC, 0, 0, SRCCOPY);
