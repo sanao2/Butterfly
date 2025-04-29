ea0ea6542618cdf650dc734de34f4f70ea5f50d5
Sanao2 <terry4690232@naver.com>
2025-04-29 10:33:44 +0900

    # 애니메이션 전용 리소스 경로 수정 작업 중

--- Butterfly/Winmain/Animation.h
+++ Butterfly/Winmain/Animation.h
@@ -3,12 +3,22 @@
 #include <vector> 
 #include <iostream>
 #include <string>
+#include "../Resource/resource.h" // Updated path to match typical project structure
 using namespace std; 
 using std::vector;
 using std::string; 
 using std::wstring; 
 
+const wstring AnimPath = L"../Resource";
+
+inline int GetAnimPath()
+{
+static int playerPath[] = {
+	IDB_PLAYER_RIGHT_ONE, 
+};
+return 0; // Added return statement to fix incomplete function
+}
+
 class Animation
 {
 };
-
