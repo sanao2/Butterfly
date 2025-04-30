ba5160e4278359416ddc981797784d8ecb7bb53a
Sanao2 <terry4690232@naver.com>
2025-04-30 14:30:41 +0900

    # 애니메이션 파일 주석 해제 # 애니메이션 파일 AnimStateFrameMap try / catch 시도 했으나 실패로 제거

--- Butterfly/Winmain/Animation.h
+++ Butterfly/Winmain/Animation.h
@@ -1,96 +1,96 @@
-//#pragma once
-//#include <windows.h>
-//#include <vector>
-//#include <iostream>
-//#include <unordered_map>
-//#include <gdiplus.h>
-//#include "UI/Timer.h"
-//#include "resource.h"
-//
-//#pragma comment(lib, "gdiplus.lib")
-//
-//using std::vector;
-//using std::wstring;
-//using std::unordered_map;
-//
-//enum Animstate
-//{
-//    PLAYER_DEFAULT,
-//    PLAYER_RIGHTWALK,
-//    PLAYER_LEFTWALK,
-//    PLAYER_DOWNWALK,
-//    PLAYER_UPWALK,
-//    PLAYER_USEPIKAX_RIGHT,
-//    PLAYER_USEPIKAX_LEFT,
-//    PLAYER_USEPIKAX_DOWN,
-//    PLAYER_USEPIKAX_UP,
-//    PLAYER_ANIMCOUNT
-//};
-//
-//const wstring AnimPath = L"../Resource/";
-//
-//struct ResourceInfo {
-//    vector<int> ImageID;
-//    ResourceInfo(std::initializer_list<int> list)
-//        : ImageID(list) {
-//    }
-//};
-//
-//unordered_map<Animstate, ResourceInfo> AnimStateFrameMap = {
-//    {PLAYER_RIGHTWALK,{IDB_PLAYER_RIGHT_IDLE, IDB_PLAYER_RIGHT_ONE, IDB_PLAYER_RIGHT_SECOND, IDB_PLAYER_RIGHT_THREE, IDB_PLAYER_RIGHT_FOUR}},
-//    {PLAYER_LEFTWALK, {IDB_LEFTWALK_ONE, IDB_LEFTWALK_SECOND, IDB_PLAYER_LEFTWALK_THREE, IDB_LEFTWALK_FOUR}},
-//    {PLAYER_DOWNWALK, {IDB_PLAYER_DOWNWALK_IDLE, IDB_PLAYER_DOWNWALK_ONE, IDB_PLAYER_DOWNWALK_SECOND, IDB_PLAYER_DOWNWALK_THREE}},
-//    {PLAYER_UPWALK, {IDB_PLAYER_UPWALK_IDLE, IDB_PLAYER_UPWALK_ONE, IDB_PLAYER_UPWALK_SECOND, IDB_PLAYER_UPWALK_THREE}},
-//    {PLAYER_USEPIKAX_RIGHT, {IDB_USEPIKAX_RIGHT_ONE, IDB_USEPIKAX_RIGHT_SECOND, IDB_USEPIKAX_RIGHT_THREE}},
-//    {PLAYER_USEPIKAX_LEFT, {IDB_USEPIKAX_LEFT_ONE, IDB_USEPIKAX_LEFT_SECOND}},
-//    {PLAYER_USEPIKAX_DOWN, {IDB_USEPIKAX_DOWN_ONE, IDB_USEPIKAX_DOWN_SECOND}},
-//    {PLAYER_USEPIKAX_UP, {IDB_USEPIKAX_UP_ONE, IDB_USEPIKAX_UP_SECOND}}
-//};
-//
-//inline const int GetAnimationFrameID(Animstate animstate, size_t frameIndex)
-//{
-//    auto it = AnimStateFrameMap.find(animstate);
-//    if (it == AnimStateFrameMap.end())
-//        return -1;
-//
-//    return it->second.ImageID[frameIndex];
-//}
-//
-//extern int g_width;
-//extern int g_height;
-//
-//class Animation
-//{
-//private:
-//    HDC clientDC = nullptr;
-//    HDC memDC = nullptr;
-//    HINSTANCE hInst = nullptr;
-//
-//    static Animstate currAnim;
-//    static Animstate prevAnim;
-//
-//    float DeltaTime = 0.0f;
-//    float frameTime = 0.0f;
-//    float TotalTime = 0.0f;
-//    int currFrame = 0;
-//
-//    Time::Timer<> timer;
-//
-//    Gdiplus::GdiplusStartupInput gsi;
-//    ULONG_PTR GdiplusToken = NULL;
-//    Gdiplus::Graphics* backDCgraphics = nullptr;
-//    HBITMAP hBitmap = nullptr;
-//    Gdiplus::Bitmap* bitmap = nullptr;
-//
-//public:
-//    Animation(HWND hwnd, int width, int height);
-//    ~Animation();
-//
-//    void createAnimation(HINSTANCE hInst, float frameTime);
-//    void Initialize();
-//    void Update();
-//    void Render(HDC drawDC);
-//
-//    void SetcurrAnimState(Animstate animstate) { currAnim = animstate; }
-//    Animstate GetcurrAnimState() { return currAnim; }
-//};
+#pragma once
+#include <windows.h>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+#include <gdiplus.h>
+#include "UI/Timer.h"
+#include "resource.h"
+
+#pragma comment(lib, "gdiplus.lib")
+
+using std::vector;
+using std::wstring;
+using std::unordered_map;
+
+enum Animstate
+{
+    PLAYER_DEFAULT,
+    PLAYER_RIGHTWALK,
+    PLAYER_LEFTWALK,
+    PLAYER_DOWNWALK,
+    PLAYER_UPWALK,
+    PLAYER_USEPIKAX_RIGHT,
+    PLAYER_USEPIKAX_LEFT,
+    PLAYER_USEPIKAX_DOWN,
+    PLAYER_USEPIKAX_UP,
+    PLAYER_ANIMCOUNT
+};
+
+const wstring AnimPath = L"../Resource/";
+
+struct ResourceInfo {
+    vector<int> ImageID;
+    ResourceInfo(std::initializer_list<int> list)
+        : ImageID(list) {
+    }
+};
+
+unordered_map<Animstate, ResourceInfo> AnimStateFrameMap = {
+    {PLAYER_RIGHTWALK,{IDB_PLAYER_RIGHT_IDLE, IDB_PLAYER_RIGHT_ONE, IDB_PLAYER_RIGHT_SECOND, IDB_PLAYER_RIGHT_THREE, IDB_PLAYER_RIGHT_FOUR}},
+    {PLAYER_LEFTWALK, {IDB_LEFTWALK_ONE, IDB_LEFTWALK_SECOND, IDB_PLAYER_LEFTWALK_THREE, IDB_LEFTWALK_FOUR}},
+    {PLAYER_DOWNWALK, {IDB_PLAYER_DOWNWALK_IDLE, IDB_PLAYER_DOWNWALK_ONE, IDB_PLAYER_DOWNWALK_SECOND, IDB_PLAYER_DOWNWALK_THREE}},
+    {PLAYER_UPWALK, {IDB_PLAYER_UPWALK_IDLE, IDB_PLAYER_UPWALK_ONE, IDB_PLAYER_UPWALK_SECOND, IDB_PLAYER_UPWALK_THREE}},
+    {PLAYER_USEPIKAX_RIGHT, {IDB_USEPIKAX_RIGHT_ONE, IDB_USEPIKAX_RIGHT_SECOND, IDB_USEPIKAX_RIGHT_THREE}},
+    {PLAYER_USEPIKAX_LEFT, {IDB_USEPIKAX_LEFT_ONE, IDB_USEPIKAX_LEFT_SECOND}},
+    {PLAYER_USEPIKAX_DOWN, {IDB_USEPIKAX_DOWN_ONE, IDB_USEPIKAX_DOWN_SECOND}},
+    {PLAYER_USEPIKAX_UP, {IDB_USEPIKAX_UP_ONE, IDB_USEPIKAX_UP_SECOND}}
+};
+
+inline const int GetAnimationFrameID(Animstate animstate, size_t frameIndex)
+{
+    auto it = AnimStateFrameMap.find(animstate);
+    if (it == AnimStateFrameMap.end())
+        return -1;
+
+    return it->second.ImageID[frameIndex];
+}
+
+extern int g_width;
+extern int g_height;
+
+class Animation
+{
+private:
+    HDC clientDC = nullptr;
+    HDC memDC = nullptr;
+    HINSTANCE hInst = nullptr;
+
+    static Animstate currAnim;
+    static Animstate prevAnim;
+
+    float DeltaTime = 0.0f;
+    float frameTime = 0.0f;
+    float TotalTime = 0.0f;
+    int currFrame = 0;
+
+    Time::Timer<> timer;
+
+    Gdiplus::GdiplusStartupInput gsi;
+    ULONG_PTR GdiplusToken = NULL;
+    Gdiplus::Graphics* backDCgraphics = nullptr;
+    HBITMAP hBitmap = nullptr;
+    Gdiplus::Bitmap* bitmap = nullptr;
+
+public:
+    Animation(HWND hwnd, int width, int height);
+    ~Animation();
+
+    void createAnimation(HINSTANCE hInst, float frameTime);
+    void Initialize();
+    void Update();
+    void Render(HDC drawDC);
+
+    void SetcurrAnimState(Animstate animstate) { currAnim = animstate; }
+    Animstate GetcurrAnimState() { return currAnim; }
+};
