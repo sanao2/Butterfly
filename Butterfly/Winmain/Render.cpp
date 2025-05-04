019a21bfcebbc511197867ede98a974e7f9cad17
Sanao2 <https://github.com/sanao2>
2025-05-04 13:47:43 +0900

    # Render 에 namespace render  추가

--- Butterfly/Winmain/Render.cpp
+++ Butterfly/Winmain/Render.cpp
@@ -1,91 +1,87 @@
 #include "Render.h"
 using namespace Input;
 
-
-Render::Render(HDC drawDC, HWND hwnd, HINSTANCE hInstance, int width, int height)
-	: hWnd(hwnd), clientSize{ width, height }, lastMoveTime(steady_clock::now()),
-	hInst(hInstance), moveMgr(Move::MoveManager(key, playerRc))
+namespace render
 {
-	swap = new Swap(hwnd, width, height);
-	animation = new Animation(drawDC, hInstance);
-}
-
-Render::~Render()
-{
-	DeleteDC(memDC);
-	delete graphics;
-	delete animation;
-	delete swap;
-}
-
-void Render::Update()
-{
-	// 이동을 위한 업데이트 
-	moveMgr.MoveUpdate();
-	
-	bool ismoving = moveMgr.IsMoving(); 
-	auto now = steady_clock::now(); 
-
-	auto elapsed = duration_cast<seconds> (now - lastMoveTime).count(); 
-	
-		ProgressAnimationKey();	
-}
-
-void Render::RenderScene(HINSTANCE hInst)
-{
-	Time::UpdateTime();
-
-	memDC = swap->GetMemDC();
-	graphics = new Gdiplus::Graphics(memDC);
-
-	// 화면 초기화 (배경을 흰색으로 채우기)
-	PatBlt(memDC, 0, 0, clientSize.x, clientSize.y, WHITENESS);
-
-	animation->Render(memDC, playerRc, graphics, 0, 0, current_frame);
-
-	// 스왑 메모리 DC에 복사 (swap 내부 메모리 DC를 가져오는 메소드 필요)
-	swap->SwapBuffers();
-
-}
-
-void Render::Moves()
-{
-	if (current_state == PLAYER_DEFAULT) return;
-	
-	auto now = steady_clock::now();
-	auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - lastMoveTime);
-	
-
-}
-POINT Render::GetBufferSize() const
-{
-	return clientSize;
-}
-void  Render::ProgressAnimationKey()
-{
-	//  키 입력으로 다음 상태 결정
-	Animstate newState = current_state;
-	if (key.IsKeyDown(VK_RIGHT))	  newState = PLAYER_RIGHTWALK;
-	else if (key.IsKeyDown(VK_LEFT))  newState = PLAYER_LEFTWALK;
-	else if (key.IsKeyDown(VK_DOWN))  newState = PLAYER_DOWNWALK;
-	else if (key.IsKeyDown(VK_UP))    newState = PLAYER_UPWALK;
-	else                              newState = PLAYER_DEFAULT;
-
-	// 상태가 변경되었을 때만 교환
-	if (newState != current_state)
+	Render::Render(HDC drawDC, HWND hwnd, HINSTANCE hInstance, int width, int height)
+		: hWnd(hwnd), clientSize{ width, height }, lastMoveTime(steady_clock::now()),
+		hInst(hInstance), moveMgr(Move::MoveManager(key, playerRc))
 	{
-		ResourceManager* resManager = animation->GetResourceManager();
-		for (auto img : resManager->AnimationFrames) {
-			delete img;
-		}	            
-		resManager->AnimationFrames.clear();
-		        
-		resManager->SetIsLoaded(false);                
-		resManager->LoadeFrames(hInst);                
+		swap = new Swap(hwnd, width, height);
+		animation = new Animation(drawDC, hInstance);
+	}
 
-		current_frame = 0;
-		current_state = newState;
-	};
+	Render::~Render()
+	{
+		DeleteDC(memDC);
+		delete graphics;
+		delete animation;
+		delete swap;
+	}
+
+	void Render::Update()
+	{
+		// 이동을 위한 업데이트 
+		moveMgr.MoveUpdate();
+
+		bool ismoving = moveMgr.IsMoving();
+		auto now = steady_clock::now();
+
+		auto elapsed = duration_cast<seconds> (now - lastMoveTime).count();
+
+		PlayerAnimationkeyInput();
+	}
+
+	void Render::RenderScene(HINSTANCE hInst)
+	{
+		Time::UpdateTime();
+
+		memDC = swap->GetMemDC();
+		graphics = new Gdiplus::Graphics(memDC);
+
+		// 화면 초기화 (배경을 흰색으로 채우기)
+		PatBlt(memDC, 0, 0, clientSize.x, clientSize.y, WHITENESS);
+
+		animation->Render(memDC, playerRc, graphics, 0, 0, current_frame);
+
+		// 스왑 메모리 DC에 복사 (swap 내부 메모리 DC를 가져오는 메소드 필요)
+		swap->SwapBuffers();
+
+	}
+
+
+	POINT Render::GetBufferSize() const
+	{
+		return clientSize;
+	}
+	void  Render::PlayerAnimationkeyInput()
+	{
+		//  키 입력으로 다음 상태 결정
+		Animstate newState = current_state;
+		if (key.IsKeyDown(VK_RIGHT))	  newState = PLAYER_RIGHTWALK;
+		else if (key.IsKeyDown(VK_LEFT))  newState = PLAYER_LEFTWALK;
+		else if (key.IsKeyDown(VK_DOWN))  newState = PLAYER_DOWNWALK;
+		else if (key.IsKeyDown(VK_UP))    newState = PLAYER_UPWALK;
+		else                              newState = PLAYER_DEFAULT;
+
+		// 상태가 변경되었을 때만 교환
+		if (newState != current_state)
+		{
+			ResourceManager* resManager = animation->GetResourceManager();
+			for (auto img : resManager->AnimationFrames) {
+				delete img;
+			}
+			resManager->AnimationFrames.clear();
+
+			resManager->SetIsLoaded(false);
+			resManager->LoadeFrames(hInst);
+
+			current_frame = 0;
+			current_state = newState;
+		};
+	}
+
 }
+
 
 
--- Butterfly/Winmain/Render.h
+++ Butterfly/Winmain/Render.h
@@ -12,30 +12,33 @@
 
 extern RECT playerRc;
 
-class Render
+namespace render
 {
-public:
-    Render(HDC drawDC, HWND hwnd, HINSTANCE hInstance, int width, int height);
-    ~Render();
+    class Render
+    {
+    public:
+        Render(HDC drawDC, HWND hwnd, HINSTANCE hInstance, int width, int height);
+        ~Render();
 
-    void Update();          // 렌더 업데이트 및 스왑 호출  
-    void RenderScene(HINSTANCE hInst);     // 실제 그리기 동작 
-    void Moves(); 
-    void ProgressAnimationKey();
-    POINT GetBufferSize() const;
+        void Update();                         // 렌더 업데이트 및 스왑 호출  
+        void RenderScene(HINSTANCE hInst);     // 실제 그리기 동작 
+        void PlayerAnimationkeyInput();           // 플레이어 이동 키 입력 
+        POINT GetBufferSize() const;
 
 
-private:
-    HWND                                           hWnd = nullptr;
-    POINT                                          clientSize = { 0, 0 };
-    HINSTANCE                                      hInst = nullptr;
+    private:
+        HWND                                           hWnd = nullptr;
+        POINT                                          clientSize = { 0, 0 };
+        HINSTANCE                                      hInst = nullptr;
 
-    Swap*                                          swap = nullptr;
-    HDC                                            memDC = nullptr;
-    Gdiplus::Graphics*                             graphics = nullptr;
-    Animation*                                     animation = nullptr;
-  
-    std::chrono::steady_clock::time_point          lastMoveTime;
-    Input::InputManager<Input::KeyboardDevice>& key = Input::InputManager<Input::KeyboardDevice>::GetInstance();
-    Move::MoveManager moveMgr;
-};
+        Swap* swap = nullptr;
+        HDC                                            memDC = nullptr;
+        Gdiplus::Graphics* graphics = nullptr;
+        Animation* animation = nullptr;
+
+        std::chrono::steady_clock::time_point          lastMoveTime;
+        Input::InputManager<Input::KeyboardDevice>& key = Input::InputManager<Input::KeyboardDevice>::GetInstance();
+        Move::MoveManager moveMgr;
+    };
+
+}
