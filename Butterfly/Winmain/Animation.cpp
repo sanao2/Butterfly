dd9a14135f00bc40446f7f34794c05be3580cc30
Sanao2 <https://github.com/sanao2>
2025-05-04 16:21:11 +0900

    # 이동 순환 오류 확인 중 # 키 입력이 지속될 시 스프라이트가 같은 방향이 아닌, 다른 방향으로 뱅글뱅글 도는 현상 수정 중

--- Butterfly/Winmain/Animation.cpp
+++ Butterfly/Winmain/Animation.cpp
@@ -29,9 +29,7 @@ void Animation::Update()
 
    bool ismoving = moveMgr->IsMoving();
    auto now = steady_clock::now();
-
-   auto elapsed = duration_cast<seconds> (now - lastMoveTime).count();
-
+   
    PlayerAnimationkeyInput();
 }
 
@@ -42,15 +40,16 @@ void Animation::Render(HDC drawDC, RECT& rect, Gdiplus::Graphics* graphics, int 
 
 void Animation::PlayerAnimationkeyInput()
 {
-
+	auto now = steady_clock::now();
+	auto elapsed = duration_cast<seconds> (now - lastMoveTime).count();
 	//  키 입력으로 다음 상태 결정
 	Animstate newState = current_state;
-
 	if (key.IsKeyDown(VK_RIGHT))	  newState = PLAYER_RIGHTWALK;
 	else if (key.IsKeyDown(VK_LEFT))  newState = PLAYER_LEFTWALK;
 	else if (key.IsKeyDown(VK_DOWN))  newState = PLAYER_DOWNWALK;
 	else if (key.IsKeyDown(VK_UP))    newState = PLAYER_UPWALK;
-	else                              newState = PLAYER_DEFAULT;
+	else if (elapsed >= 1.0f)		  newState = PLAYER_DEFAULT;
+	else return; 
 
 	// 상태가 변경되었을 때만 교환
 	if (newState != current_state)
@@ -65,6 +64,7 @@ void Animation::PlayerAnimationkeyInput()
 
 		current_frame = 0;
 		current_state = newState;
+		timer.Reset();
 	};
 }
 
--- Butterfly/Winmain/Render.cpp
+++ Butterfly/Winmain/Render.cpp
@@ -39,7 +39,7 @@ void Render::RenderScene(HINSTANCE hInst)
 	// 화면 초기화 (배경을 흰색으로 채우기)
 	PatBlt(memDC, 0, 0, clientSize.x, clientSize.y, WHITENESS);
 
-	animation->Render(memDC, playerRc, graphics, 0, 0, current_frame);
+	animation->Render(memDC, playerRc, graphics, (playerRc.right - playerRc.left) / 2, (playerRc.bottom - playerRc.top) / 2, current_frame);
 
 	// 스왑 메모리 DC에 복사 (swap 내부 메모리 DC를 가져오는 메소드 필요)
 	swap->SwapBuffers();
