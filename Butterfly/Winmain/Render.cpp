4830eb20777889de99c94f315bde1280a6af027d
Sanao2 <https://github.com/sanao2>
2025-05-04 15:01:54 +0900

    Revert "# Render 에 namespace render  추가"
    This reverts commit 019a21bfcebbc511197867ede98a974e7f9cad17.

--- Butterfly/Winmain/Render.cpp
+++ Butterfly/Winmain/Render.cpp
@@ -1,87 +1,91 @@
 #include "Render.h"
 using namespace Input;
 
-namespace render
+
+Render::Render(HDC drawDC, HWND hwnd, HINSTANCE hInstance, int width, int height)
+	: hWnd(hwnd), clientSize{ width, height }, lastMoveTime(steady_clock::now()),
+	hInst(hInstance), moveMgr(Move::MoveManager(key, playerRc))
 {
-	Render::Render(HDC drawDC, HWND hwnd, HINSTANCE hInstance, int width, int height)
-		: hWnd(hwnd), clientSize{ width, height }, lastMoveTime(steady_clock::now()),
-		hInst(hInstance), moveMgr(Move::MoveManager(key, playerRc))
-	{
-		swap = new Swap(hwnd, width, height);
-		animation = new Animation(drawDC, hInstance);
-	}
+	swap = new Swap(hwnd, width, height);
+	animation = new Animation(drawDC, hInstance);
+}
 
-	Render::~Render()
-	{
-		DeleteDC(memDC);
-		delete graphics;
-		delete animation;
-		delete swap;
-	}
+Render::~Render()
+{
+	DeleteDC(memDC);
+	delete graphics;
+	delete animation;
+	delete swap;
+}
 
-	void Render::Update()
-	{
-		// 이동을 위한 업데이트 
-		moveMgr.MoveUpdate();
+void Render::Update()
+{
+	// 이동을 위한 업데이트 
+	moveMgr.MoveUpdate();
+	
+	bool ismoving = moveMgr.IsMoving(); 
+	auto now = steady_clock::now(); 
 
-		bool ismoving = moveMgr.IsMoving();
-		auto now = steady_clock::now();
+	auto elapsed = duration_cast<seconds> (now - lastMoveTime).count(); 
+	
+		ProgressAnimationKey();	
+}
 
-		auto elapsed = duration_cast<seconds> (now - lastMoveTime).count();
+void Render::RenderScene(HINSTANCE hInst)
+{
+	Time::UpdateTime();
 
-		PlayerAnimationkeyInput();
-	}
+	memDC = swap->GetMemDC();
+	graphics = new Gdiplus::Graphics(memDC);
 
-	void Render::RenderScene(HINSTANCE hInst)
-	{
-		Time::UpdateTime();
+	// 화면 초기화 (배경을 흰색으로 채우기)
+	PatBlt(memDC, 0, 0, clientSize.x, clientSize.y, WHITENESS);
 
-		memDC = swap->GetMemDC();
-		graphics = new Gdiplus::Graphics(memDC);
+	animation->Render(memDC, playerRc, graphics, 0, 0, current_frame);
 
-		// 화면 초기화 (배경을 흰색으로 채우기)
-		PatBlt(memDC, 0, 0, clientSize.x, clientSize.y, WHITENESS);
-
-		animation->Render(memDC, playerRc, graphics, 0, 0, current_frame);
-
-		// 스왑 메모리 DC에 복사 (swap 내부 메모리 DC를 가져오는 메소드 필요)
-		swap->SwapBuffers();
-
-	}
-
-
-	POINT Render::GetBufferSize() const
-	{
-		return clientSize;
-	}
-	void  Render::PlayerAnimationkeyInput()
-	{
-		//  키 입력으로 다음 상태 결정
-		Animstate newState = current_state;
-		if (key.IsKeyDown(VK_RIGHT))	  newState = PLAYER_RIGHTWALK;
-		else if (key.IsKeyDown(VK_LEFT))  newState = PLAYER_LEFTWALK;
-		else if (key.IsKeyDown(VK_DOWN))  newState = PLAYER_DOWNWALK;
-		else if (key.IsKeyDown(VK_UP))    newState = PLAYER_UPWALK;
-		else                              newState = PLAYER_DEFAULT;
-
-		// 상태가 변경되었을 때만 교환
-		if (newState != current_state)
-		{
-			ResourceManager* resManager = animation->GetResourceManager();
-			for (auto img : resManager->AnimationFrames) {
-				delete img;
-			}
-			resManager->AnimationFrames.clear();
-
-			resManager->SetIsLoaded(false);
-			resManager->LoadeFrames(hInst);
-
-			current_frame = 0;
-			current_state = newState;
-		};
-	}
+	// 스왑 메모리 DC에 복사 (swap 내부 메모리 DC를 가져오는 메소드 필요)
+	swap->SwapBuffers();
 
 }
 
+void Render::Moves()
+{
+	if (current_state == PLAYER_DEFAULT) return;
+	
+	auto now = steady_clock::now();
+	auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - lastMoveTime);
+	
+
+}
+POINT Render::GetBufferSize() const
+{
+	return clientSize;
+}
+void  Render::ProgressAnimationKey()
+{
+	//  키 입력으로 다음 상태 결정
+	Animstate newState = current_state;
+	if (key.IsKeyDown(VK_RIGHT))	  newState = PLAYER_RIGHTWALK;
+	else if (key.IsKeyDown(VK_LEFT))  newState = PLAYER_LEFTWALK;
+	else if (key.IsKeyDown(VK_DOWN))  newState = PLAYER_DOWNWALK;
+	else if (key.IsKeyDown(VK_UP))    newState = PLAYER_UPWALK;
+	else                              newState = PLAYER_DEFAULT;
+
+	// 상태가 변경되었을 때만 교환
+	if (newState != current_state)
+	{
+		ResourceManager* resManager = animation->GetResourceManager();
+		for (auto img : resManager->AnimationFrames) {
+			delete img;
+		}	            
+		resManager->AnimationFrames.clear();
+		        
+		resManager->SetIsLoaded(false);                
+		resManager->LoadeFrames(hInst);                
+
+		current_frame = 0;
+		current_state = newState;
+	};
+}
 
 
